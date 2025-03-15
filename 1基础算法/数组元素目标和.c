d084f82349d5b479ae2a2c84dd4955273cbd66a1
houmiao <houmiao561@gmail.com>
2025-03-15 16:27:02 +0800

    双指针

--- 1基础算法/数组元素目标和.c
+++ 1基础算法/数组元素目标和.c
@@ -24,3 +24,27 @@ void TargetAdd( int A[],int lenA, int B[], int lenB, int target ) {
         }
     } 
 }
+
+
+// C++
+#include <iostream>
+#include <algorithm>
+using namespace std;
+const int MAXLEN = 100010;
+int n,m,x;
+int a[MAXLEN];
+int b[MAXLEN];
+
+int main() {
+    cin >> n >> m >> x;
+    for ( int i = 0; i<n; i++ ) cin >> a[i];
+    for ( int i = 0; i<m; i++ ) cin >> b[i];
+    
+    int i = 0, j = m-1;
+    while( a[i] + b[j] != x ) {
+        if ( a[i] + b[j] > x ) j--;
+        if ( a[i] + b[j] < x ) i++;
+    }
+    cout << i << ' ' << j;
+    return 0;
+}
