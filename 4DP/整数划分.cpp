f825056217e6bd1c99c9320d6dbc0cf40162efd0
houmiao <houmiao561@gmail.com>
2025-03-16 21:47:27 +0800

    。

--- 4DP/整数划分.cpp
+++ 4DP/整数划分.cpp
@@ -16,3 +16,34 @@ int main() {
     }
     cout << f[n];
 }
+
+
+
+
+// f[i][j] 表示前i个整数（1,2…,i）恰好拼成j的方案数
+// 可以理解为完全背包问题，i是物品的体积，j是现有背包的大小
+// f[i][j] = f[i - 1][j] + f[i][j - i]
+
+#include <iostream>
+using namespace std;
+const int N = 1e3 + 7, mod = 1e9 + 7;
+
+int n;
+int f[N][N];
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i <= n; i ++) {
+        f[i][0] = 1; // 容量为0时，前 i 个物品全不选也是一种方案
+    }
+
+    for (int i = 1; i <= n; i ++) {
+        for (int j = 0; j <= n; j ++) {
+            f[i][j] = f[i - 1][j] % mod; // 特殊 f[0][0] = 1
+            if (j >= i) f[i][j] = (f[i - 1][j] + f[i][j - i]) % mod;
+        }
+    }
+
+    cout << f[n][n] << endl;
+}
